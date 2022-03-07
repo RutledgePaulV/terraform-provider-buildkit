92ea7143a67ba1527190855f35b9ad3b9864443c
Paul Rutledge <paul.rutledge@rate.com>
2022-03-07 08:18:21 -0600

    initial implementation

--- buildkit/buildkit_image_funcs.go
+++ buildkit/buildkit_image_funcs.go
@@ -2,14 +2,50 @@ package buildkit
 
 import (
 	"context"
+	"fmt"
 	"github.com/docker/cli/cli/command/image/build"
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/pkg/archive"
+	"github.com/docker/go-units"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"io"
+	"os"
 )
+
+func getCompiledTags(data *schema.ResourceData) []string {
+	tags := make([]string, 0)
+	publish_targets := data.Get("publish_targets").([]map[string]string)
+	for _, x := range publish_targets {
+		tags = append(tags, fmt.Sprintf("%s/%s:%s", x["registry"], x["name"], x["tag"]))
+	}
+	return tags
+}
+
+func getCompiledOutputs(data *schema.ResourceData) []types.ImageBuildOutput {
+	outputs := make([]types.ImageBuildOutput, 0)
+	publish_targets := data.Get("publish_targets").([]map[string]string)
+	for _, x := range publish_targets {
+		outputs = append(outputs, types.ImageBuildOutput{
+			Type: "image",
+			Attrs: map[string]string{
+				"name": fmt.Sprintf("%s/%s:%s", x["registry"], x["name"], x["tag"]),
+				"push": "true",
+			},
+		})
+	}
+	return outputs
+}
+
+func getCompiledBuildArgs(data *schema.ResourceData) map[string]*string {
+
+}
+
+func getCompiledAuthConfigs(meta interface{}) map[string]types.AuthConfig {
+
+}
 
 func createImage(context context.Context, data *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	diagnostics := make(diag.Diagnostics, 0)
@@ -25,7 +61,7 @@ func createImage(context context.Context, data *schema.ResourceData, meta interf
 
 	defer cli.Close()
 
-	contextDir := ""
+	contextDir := data.Get("context").(string)
 
 	excludePatterns, err := build.ReadDockerignore(contextDir)
 
@@ -42,8 +78,68 @@ func createImage(context context.Context, data *schema.ResourceData, meta interf
 
 	defer tarHandle.Close()
 
+	dockerFilePath := data.Get("dockerfile").(string)
+
+	dockerFileHandle, err := os.Open(dockerFilePath)
+
+	if err != nil {
+		return append(diagnostics, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "Encountered error opening dockerfile.",
+		})
+	}
+
+	defer dockerFileHandle.Close()
+
+	dockerFileText, err := io.ReadAll(dockerFileHandle)
+
+	if err != nil {
+		return append(diagnostics, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "Encountered error reading dockerfile.",
+		})
+	}
+
+	outputs := getCompiledOutputs(data)
+
 	options := types.ImageBuildOptions{
-		Labels: data.Get("labels").(map[string]string),
+		// options influenced by terraform settings
+		Tags:        getCompiledTags(data),
+		Dockerfile:  string(dockerFileText),
+		BuildArgs:   getCompiledBuildArgs(data),
+		AuthConfigs: getCompiledAuthConfigs(meta),
+		Context:     tarHandle,
+		Labels:      data.Get("labels").(map[string]string),
+		Version:     types.BuilderBuildKit,
+		Outputs:     outputs,
+
+		// unexposed options
+		Squash:         false,
+		CacheFrom:      make([]string, 0),
+		SecurityOpt:    make([]string, 0),
+		ExtraHosts:     make([]string, 0),
+		Ulimits:        make([]*units.Ulimit, 0),
+		SuppressOutput: false,
+		RemoteContext:  "",
+		NoCache:        false,
+		Remove:         false,
+		ForceRemove:    false,
+		PullParent:     false,
+		Isolation:      container.IsolationDefault,
+		CPUSetCPUs:     "",
+		CPUSetMems:     "",
+		CPUShares:      0,
+		CPUQuota:       0,
+		CPUPeriod:      0,
+		Memory:         0,
+		MemorySwap:     0,
+		CgroupParent:   "",
+		NetworkMode:    "",
+		ShmSize:        0,
+		Target:         "",
+		SessionID:      "",
+		Platform:       "",
+		BuildID:        "",
 	}
 
 	response, err := cli.ImageBuild(context, tarHandle, options)
